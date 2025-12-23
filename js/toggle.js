56b230f448f6a53fb37800fcac6f2d6404a3bec1
theworld <tamangsandip609@gmail.com>
2025-12-23 08:44:24 +0545

    Refactor sticky nav to separate file and update script references
    Moved the sticky navigation logic from script.js to a new sticky_nav.js file. Renamed script.js to toggle.js to better reflect its functionality. Updated all HTML files to reference the new toggle.js and, where needed, sticky_nav.js scripts.

--- js/toggle.js
+++ js/toggle.js
@@ -54,31 +54,6 @@ function toggleMenu() {
     });
 }
 
-function stickyNav() {
-    const header = document.querySelector('.header');
-    const nav = document.querySelector('.nav');
-    const headerHeight = header.offsetHeight;
-    let ticking = false;
-    window.addEventListener('scroll', () => {
-        if(!ticking) {
-            window.requestAnimationFrame(() => {
-                if (window.scrollY > headerHeight) {
-                    nav.classList.add("sticky");
-                } else {
-                    nav.classList.remove('sticky');
-                }
-                ticking = false;
-            });
-            ticking = true;
-        }
-    });
-}
-
 document.addEventListener('DOMContentLoaded', () => {
     toggleMenu();
-    stickyNav();
 });
-
-
-
-
